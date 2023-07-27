93dd1a668c26d06c54f14cb89a54ee400486eec9
Jonatan Lifshits <djonli2003@gmail.com>
2023-07-27 18:14:21 +0300

    Initial commit: migrations

---
+++ MigrationsHW/Db.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationsHW
+{
+    internal class Db : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-TO8EGSF;Database=p01_intro;Trusted_Connection=True;Encrypt=False;");
+        }
+    }
+}
---
+++ MigrationsHW/Migrations/20230727134653_product_adding_price.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MigrationsHW.Migrations
+{
+    /// <inheritdoc />
+    public partial class product_adding_price : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Products",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+        }
+    }
+}
